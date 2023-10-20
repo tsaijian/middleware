fd892812954b0bad7cdd2825d781ed9af1816bd5
Andrew Walker <awalker@ixsystems.com>
2023-10-20 11:01:11 -0700

    Fix test

--- tests/api2/test_account_password_policies.py
+++ tests/api2/test_account_password_policies.py
@@ -37,12 +37,13 @@ def test_password_reset(grant_users_password_reset_privilege):
     with user({
         'username': USER,
         'full_name': USER,
+        'group_create': True,
         'home': '/var/empty',
         'shell': '/usr/bin/bash',
         'password_aging_enabled': True,
         'ssh_password_enabled': True,
         'password': PASSWD1
-    }):
+    }) as u:
         ssh('pwd', user=USER, password=PASSWD1)
 
         # `user.password_reset` should be allowed
@@ -57,3 +58,9 @@ def test_password_reset(grant_users_password_reset_privilege):
                 c.call('user.reset_password', PASSWD2, PASSWD1)
 
         assert PASSWORD_REUSE_ERR in str(ve), str(ve)
+
+        # Make sure we can change back to password with nonsense
+        # turned off
+        c.call('user.update', u['id'], {'password_aging_enabled': False})
+        with client(auth=(USER, PASSWD2)) as c:
+            c.call('user.reset_password', PASSWD2, PASSWD1)
